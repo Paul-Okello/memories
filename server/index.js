2d4bfc6c6604ae6528aa7a6b71b24f1e0a3d59f6
Paul Okello <paulokello273@gmail.com>
2020-12-09 18:48:18 +0300

    Server and database connected 🚀

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+//general 🕵
+app.use(
+  bodyParser.json({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+app.use(cors());
+
+//DataBase Setup
+const CONNECTION_URL =
+  "mongodb+srv://admin:FMcDNH03td1b0Fmy@cluster0.gzz0g.mongodb.net/memories?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server Running on port : ${PORT} and MongoDB Connected 🚀🚀🚀`
+      )
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);

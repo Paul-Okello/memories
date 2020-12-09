63a1ef294e30949950c0327ec86fccd9c74c09e0
Paul Okello <paulokello273@gmail.com>
2020-12-09 21:53:22 +0300

    Combine Reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,10 @@
-import { combineReducers } from "redux";
-import posts from "./posts";
-
-export default combineReducers({
-  posts,
-});
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return posts;
+    case "CREATE":
+      return posts;
+    default:
+      return posts;
+  }
+};

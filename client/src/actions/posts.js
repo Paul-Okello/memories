581d1698475166cca6644abd2332091a46efecf5
Paul Okello <paulokello273@gmail.com>
2020-12-10 10:27:06 +0300

    Making data iterable

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -12,7 +12,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
-    const [data] = await api.createPost(post);
+    const { data } = await api.createPost(post);
 
     dispatch({ type: "CREATE", payload: data });
   } catch (error) {

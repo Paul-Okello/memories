5be6924cfcc2dd0912b01af56cb827d6e6d5f393
Paul Okello <paulokello273@gmail.com>
2020-12-10 09:37:28 +0300

    Provider

--- client/src/index.js
+++ client/src/index.js
@@ -10,11 +10,9 @@ import reducers from "./reducers";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 

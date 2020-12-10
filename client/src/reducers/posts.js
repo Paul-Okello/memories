273e3f220d012b8e3735ca3d2be0acf569d15a68
Paul Okello <paulokello273@gmail.com>
2020-12-10 12:29:06 +0300

    update reducer ⛑⛑⛑⛑

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,7 +4,10 @@ export default (posts = [], action) => {
       return action.payload;
     case "CREATE":
 
-      return [...posts, action.payload];
+    case "UPDATE":
+      return posts.map((post) =>
+        posts._id === action.payload._id ? action.payload : post
+      );
 
     default:
       return posts;

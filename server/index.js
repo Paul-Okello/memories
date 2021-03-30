8b1e7a2b82e58fa45f58dd888fcafc16418400dd
Paul Okello <paulokello273@gmail.com>
2021-03-30 12:39:51 +0300

    Debugging the routes typo

--- server/index.js
+++ server/index.js
@@ -3,30 +3,23 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
-app.use(cors());
-//general 🕵
 
-app.use(
-  bodyParser.json({
-    limit: "30mb",
-    extended: true,
-  })
-);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello to Memories API by Paul Okello");
 });
-app.use(
-  bodyParser.urlencoded({
-    limit: "30mb",
-    extended: true,
-  })
-);
-app.use("/posts", postRoutes);
+
 //DataBase Setup
 
 const PORT = process.env.PORT || 5000;

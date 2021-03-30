04bbe9b897eeff41aa28c50128e29510cab628ab
Paul Okello <paulokello273@gmail.com>
2021-03-30 08:40:12 +0300

    Minor Debugging changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { CREATE, DELETE, FETCH_ALL, UPDATE } from "../constants/actionTypes";
 
-export default (posts = [], action) => {
+const posts = (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
@@ -17,3 +17,5 @@ export default (posts = [], action) => {
       return posts;
   }
 };
+
+export default posts;

49bb4f005960c285aaefcdde0fe545c51ff14e3d
Paul Okello <paulokello273@gmail.com>
2021-03-31 07:23:35 +0300

    Added axios interceptor

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,16 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 // const url = "https://memorie-mern.herokuapp.com/posts";
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 
 export const createPost = (newPost) => API.post("/posts", newPost);

1fe523c18eb024e3a7e482f1b0aac49294623956
Paul Okello <paulokello273@gmail.com>
2021-03-31 07:24:30 +0300

    Logout, jwt authentication

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,58 +1,66 @@
-import React, { useEffect, useState } from "react";
-import { AppBar, Typography, Toolbar, Button, Avatar } from "@material-ui/core";
-import useStyles from "./style";
+import React, { useState, useEffect } from "react";
+import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core";
 import { Link, useHistory, useLocation } from "react-router-dom";
-import memories from "../../images/memories.png";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
+
+import memories from "../../images/memories.png";
+import * as actionType from "../../constants/actionTypes";
+import useStyles from "./style";
 
 const Navbar = () => {
-  const classes = useStyles();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const dispatch = useDispatch();
-  const history = useHistory();
   const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
+
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push("/auth");
+
+    setUser(null);
+  };
 
   useEffect(() => {
     const token = user?.token;
-    // JWT
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
-  const logout = () => {
-    dispatch({
-      type: "LOGOUT",
-    });
-    history.push("/");
-    setUser(null);
-  };
-
   return (
-    <AppBar position="static" color="inherit" className={classes.appBar}>
+    <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
         <Typography
           component={Link}
           to="/"
+          className={classes.heading}
           variant="h2"
           align="center"
-          className={classes.heading}
         >
           Memories
-          <img src={memories} alt="memories" height="60" />
         </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
               className={classes.purple}
-              alt={user.result.name}
-              src={user.result.imageUrl}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
             >
-              {user.result.name.charAt(0)}
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
-              {user.result.name}
+              {user?.result.name}
             </Typography>
             <Button
               variant="contained"
